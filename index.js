21a6b9ac46ab2150ca5ea25dbce6f79747dcc4c7
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-08-30 18:41:04 -0400

    commit tree 5c1acad2ef70fe8bbaf533e58f2e3f25944507f8

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob';
+//var bestCustomer = 'not bob';
+
+
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+    
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';   
+}
+
+function overwriteBestCustomer(randomName) {
+    bestCustomer = randomName;
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = 'always bobby';
+    leastFavoriteCustomer = 'always bobby';
+}
